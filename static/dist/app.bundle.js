24a292bdb814300c03643c6878af6fc1fd4882a1
Eyad BASHA <eyad.esgi@gmail.com>
2020-11-19 11:00:48 +0100

    logo

--- static/dist/app.bundle.js
+++ static/dist/app.bundle.js
@@ -27437,11 +27437,34 @@ document.onkeydown = function (evt) {
   !*** ./src/js/social.js ***!
   \**************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-// Whatsapp
+/* WEBPACK VAR INJECTION */(function($) {// WHATSAPP
 // https://api.whatsapp.com/send?text=[post-title] [post-url]
-//
+// FACEBOK 
+// https://www.facebook.com/sharer.php?u=[post-url]
+// TWITTER
+// https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
+// LINKEDIN 
+// https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+const facebookBtn = $('.facebook-btn');
+const twitterBtn = $('.twitter-btn');
+const linkedinkBtn = $('.linkedin-btn');
+const whatsappBtn = $('.whatsapp-btn');
+
+function init() {
+  let url = encodeURI(document.location.href);
+  let post_title = encodeURI($('#post_title').text());
+  let image = encodeURI($('#post_img').attr('src'));
+  let description = encodeURI($('#post_content').text());
+  facebookBtn.attr('href', `https://www.facebook.com/sharer.php?u=${url}&text=${post_title}&image=${image}`);
+  twitterBtn.attr('href', `https://twitter.com/share?url=${url}&text=${post_title}`);
+  linkedinkBtn.attr('href', `https://www.linkedin.com/shareArticle?url=${url}&title=${post_title}`);
+  whatsappBtn.attr('href', `https://api.whatsapp.com/send?text=${post_title} ${url}`);
+}
+
+init();
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
 
 /***/ }),
 
